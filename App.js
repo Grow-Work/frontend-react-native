1d598d7a56a7195afa4af3af9e2a957288be886a
Dionne <mylife33@hotmail.com>
2022-05-29 23:00:25 -0500

    one more icon change

--- App.js
+++ App.js
@@ -11,7 +11,7 @@ import ProfileListScreen from './src/screens/ProfileListScreen';
 import {Provider as AuthProvider} from './src/context/AuthContext'
 import { setNavigator } from './src/navigationRef';
 import ResolveAuthScreen from './src/screens/ResolveAuthScreen';
-import {MaterialIcons, MaterialCommunityIcons, FontAwesome5} from '@expo/vector-icons'
+import {MaterialIcons, Ionicons, MaterialCommunityIcons, FontAwesome5} from '@expo/vector-icons'
 import CompanyListScreen from './src/screens/CompanyListScreen';
 import CompanyDetailScreen from './src/screens/CompanyDetailScreen';
 import JobListScreen from './src/screens/JobListScreen';
@@ -24,7 +24,7 @@ const ProfileListFlow = createStackNavigator({
 
 ProfileListFlow.navigationOptions = {
   title: 'Newbs',
-    tabBarIcon: <MaterialCommunityIcons name="account-group-outline" size={20} />
+    tabBarIcon: <Ionicons name="people-outline" size={20} />
 }
 
 const CompanyListFlow = createStackNavigator({
