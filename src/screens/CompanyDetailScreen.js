d66d5296f18ac0706793932055a8af634b64717a
Dionne <mylife33@hotmail.com>
2022-06-09 16:01:55 -0500

    job, newb, company details populate on detail screens.

--- src/screens/CompanyDetailScreen.js
+++ src/screens/CompanyDetailScreen.js
@@ -1,10 +1,38 @@
-import React from 'react'
-import { View, StyleSheet, Text } from 'react-native'
+import React, {useContext} from 'react'
+import { StyleSheet, Text } from 'react-native'
+import { Context as DataContext } from '../context/DataContext'
+import { SafeAreaView } from 'react-native-safe-area-context'
 
-const NewbProfileDetailScreen = () => {
-    return <Text style={{fontSize:48}} >Company Detail Screen</Text>
+const NewbProfileDetailScreen = ({navigation}) => {
+
+    const {state} = useContext(DataContext)
+    const _id = navigation.getParam('_id')
+
+    const company = state.find(t => t._id === _id)
+
+    return (
+        <SafeAreaView style={styles.container} forceInset={{top: 'always'}}>
+        <Text style={styles.header} >{company.name}</Text>
+        <Text style={styles.text} >{company.location}</Text>
+        <Text style={styles.text} >And other data here...</Text>
+        </SafeAreaView>
+        )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    header: {
+        fontSize: 30,
+        marginBottom: 10
+    },
+    container: {
+        margin: 15,
+        flex: 1,
+        marginTop: 15
+    },
+    text: {
+        fontSize: 20,
+        marginBottom: 5
+    }
+})
 
 export default NewbProfileDetailScreen
